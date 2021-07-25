432965b94b8c07aa27e11a613316aafe21990f27
bonaventuragal <bonaventuragal@gmail.com>
2021-07-25 21:55:46 +0700

    Change bullet size

--- bullet.cpp
+++ bullet.cpp
@@ -5,7 +5,7 @@ Bullet::Bullet() {
 }
 
 QRectF Bullet::boundingRect() const {
-	return QRectF(0, 0, 10, 4);
+	return QRectF(0, 0, 15, 3);
 }
 
 void Bullet::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
